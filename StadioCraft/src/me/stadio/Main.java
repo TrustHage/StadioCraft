a6ad893a96a10818ae196bc706badc50ba589976
mcamps <mcamps@192.168.1.25>
2014-08-25 22:26:23 +0200

    Mick added some stuff :)

--- StadioCraft/src/me/stadio/Main.java
+++ StadioCraft/src/me/stadio/Main.java
@@ -2,26 +2,36 @@ package me.stadio;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin
-{
+public class Main extends JavaPlugin {
 
 	public static Main plugin;
-	
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
+		plugin = this;
+
 		getLogger().info("The StadioCraft Plugin is enabled!");
+
+		getCommands();
+
+		initializeConfig();
+	}
+
+	@Override
+	public void onDisable() {
+		saveConfig();
+	}
+
+	private void initializeConfig() {
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+
+	private void getCommands() {
 		getCommand("regen").setExecutor(new RegenCommand());
 		getCommand("fury").setExecutor(new FuryCommand());
 		getCommand("rage").setExecutor(new RageCommand());
 		getCommand("tnt").setExecutor(new TNTCommand());
-		getConfig().options().copyDefaults(true);
-		saveConfig();
-		}
-	
-	@Override
-	public void onDisable()
-	{
-		saveConfig();
 	}
+
 }
